62a919d0fe0492cbfb0ad4241db138086d67b104
MrCosmo <mail@mrcosmo.info>
2022-06-25 09:54:07 +0200

    fixed spacing

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
-  <v-app>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
+  	<v-app>
+    	<v-main>
+      		<ServerList />
+    	</v-main>
+  	</v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ServerList from './components/ServerList.vue'
 
 export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
+  	name: 'App',
+  	components: {
+  		ServerList,
+  	},
 }
 </script>
---
+++ src/components/ServerList.vue
@@ -0,0 +1,18 @@
+<template>
+  	<v-container>
+    	<v-row class="text-center">
+			<v-col>
+				Hello
+			</v-col>
+		</v-row>
+  	</v-container>
+</template>
+
+<script>
+export default {
+  	name: 'ServerList',
+  	data: () => ({
+  		servers: []
+  	}),
+}
+</script>
